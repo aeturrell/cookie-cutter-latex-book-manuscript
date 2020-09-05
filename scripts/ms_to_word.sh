e877e36894f8aafbd1b4040e3dbfa94cd6ad80f6
aeturrell <a.e.turrell@gmail.com>
2020-09-05 17:38:38 +0100

    wrking word TOC

--- scripts/ms_to_word.sh
+++ scripts/ms_to_word.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pandoc -s -N --reference-doc ref.docx -F pandoc-crossref -Mchapters book_compiler.tex --bibliography=book_bibliography.bib --csl=ieee.csl -o exported_word/book.docx
+pandoc -s -N --toc --toc-depth=1 --reference-doc ref.docx -F pandoc-crossref -Mchapters book_compiler.tex --bibliography=book_bibliography.bib --csl=ieee.csl -o exported_word/book.docx
